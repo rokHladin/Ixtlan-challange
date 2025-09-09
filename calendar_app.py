5d2d2ca593eee9cc44f3e0c5ad103535392ad5e5
rokHladin <rok.hladin@gmail.com>
2025-09-09 10:00:58 +0200

    Add month grid

--- calendar_app.py
+++ calendar_app.py
@@ -1,12 +1,69 @@
 import tkinter as tk
 from tkinter import ttk
 
+import calendar
+import datetime
+
 class Calendar:
     
     def __init__(self):
         self.root = tk.Tk()
+        
+        self.current_month = datetime.datetime.now().month
+        self.current_year = datetime.datetime.now().year
+        
+        self.create_widgets()
+        self.update_calendar()
+        
+    def create_widgets(self):
+        main_frame = ttk.Frame(self.root, padding="10")
+        main_frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        
+        self.calendar_frame = ttk.Frame(main_frame, relief='solid', borderwidth=1)
+        self.calendar_frame.grid(row=2, column=0, columnspan=3, sticky=(tk.W, tk.E, tk.N, tk.S))
+        
+            
+        self.create_calendar_grid()
+            
+    def create_calendar_grid(self):
+        # Create cells for days
+        self.day_cells = []
+        # 6 weeks, because day can start on sunday
+        for row in range(1, 7):
+            week_cells = []
+            # 7 days in a week
+            for col in range(7):
+                cell = tk.Label(self.calendar_frame, text="", font=('Arial', 12),
+                            relief='raised', borderwidth=1, width=6, height=3)
+                cell.grid(row=row, column=col, sticky=(tk.W, tk.E, tk.N, tk.S), padx=1, pady=1)
+                week_cells.append(cell)
+            self.day_cells.append(week_cells)
+            
+    def update_calendar(self):
+        for week in self.day_cells:
+            for cell in week:
+                cell.config(text="", bg='white', fg='black')
+        
+        month_days = calendar.monthcalendar(self.current_year, self.current_month)
+        
+        print(month_days)
+        
+        for week_idx, week in enumerate(month_days):
+            if week_idx >= len(self.day_cells):
+                break
+                
+            for day_idx, day in enumerate(week):
+                if day == 0:
+                    continue # Empty day
+                
+                cell = self.day_cells[week_idx][day_idx]
+                cell.config(text=str(day))
+                
+                cell.config(bg='white', fg='black')
+    
     def run(self):
         self.root.mainloop()
+        
 
 def main():
     try:
